c970859b4ec29198f15875cad2af67b8af383d2f
Neehar Patil <pluckynumbat@gmail.com>
2024-09-29 14:16:11 +0530

    remove more unused (as of now) stuff

--- linkedListHelper/linkedListHelper.go
+++ linkedListHelper/linkedListHelper.go
@@ -8,19 +8,11 @@ import (
 func main() {
 	fmt.Println("let's help you with a linked list")
 	basicAddStuff()
-	
+
 }
 
 func basicAddStuff() {
 	list := linkedListLibrary.ConstructFromValues("1", "2", "3")
 	fmt.Printf("here's your list: %s \n", list.GetStringForm())
-	fmt.Println("")
-}
-
-func basicRemovalStuff() {
-	list := linkedListLibrary.ConstructNode("2"))
-	linkedList.AddToBeginning("1")
-	linkedList.AddToEnd("3")
-	fmt.Printf("here's your list: %s", linkedList.GetStringForm())
 	fmt.Println("")
 }
