89a475806406974349f32b0fdb0f1a6474e78c06
Neehar Patil <pluckynumbat@gmail.com>
2024-10-07 13:33:35 +0530

    clean up the linked list helper

--- linkedListHelper/linkedListHelper.go
+++ linkedListHelper/linkedListHelper.go
@@ -7,66 +7,7 @@ import (
 
 func main() {
 	fmt.Println("let's help you with a linked list")
-	basicAddStuff()
-	basicRemovalStuff()
-	removeAtEndStuff()
-	removeAtBeginningStuff()
 	basicCopyStuff()
-}
-
-func basicAddStuff() {
-	list := linkedListLibrary.ConstructFromValues("1", "2", "3")
-	fmt.Printf("here's your list: %s \n", list.GetStringForm())
-	fmt.Println("")
-}
-
-func basicRemovalStuff() {
-	list := linkedListLibrary.ConstructFromValues("w", "h", "a", "t")
-	fmt.Printf("here's your list: %s \n", list.GetStringForm())
-
-	list.RemoveValue("i")
-	list.RemoveValue("w")
-	fmt.Printf("here's your list: %s \n", list.GetStringForm())
-
-	list.RemoveValue("a")
-	fmt.Printf("here's your list: %s \n", list.GetStringForm())
-
-	list.RemoveValue("t")
-	fmt.Printf("here's your list: %s \n", list.GetStringForm())
-
-	list.RemoveValue("h")
-	fmt.Printf("here's your list: %s \n", list.GetStringForm())
-
-	list.RemoveValue("b")
-	fmt.Println("")
-}
-
-func removeAtEndStuff() {
-	list := linkedListLibrary.ConstructFromValues("w", "h", "a", "t")
-	fmt.Printf("here's your list: %s \n", list.GetStringForm())
-
-	for !list.IsEmpty() {
-		list.RemoveAtEnd()
-		fmt.Printf("here's your list: %s \n", list.GetStringForm())
-	}
-
-	list.RemoveAtEnd()
-	fmt.Printf("here's your list: %s \n", list.GetStringForm())
-	fmt.Println("")
-}
-
-func removeAtBeginningStuff() {
-	list := linkedListLibrary.ConstructFromValues("w", "h", "a", "t")
-	fmt.Printf("here's your list: %s \n", list.GetStringForm())
-
-	for !list.IsEmpty() {
-		list.RemoveAtBeginning()
-		fmt.Printf("here's your list: %s \n", list.GetStringForm())
-	}
-
-	list.RemoveAtBeginning()
-	fmt.Printf("here's your list: %s \n", list.GetStringForm())
-	fmt.Println("")
 }
 
 func basicCopyStuff() {
