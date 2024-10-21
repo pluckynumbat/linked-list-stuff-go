69f995cea0512e659b3e3587b6defda28352d506
Neehar Patil <pluckynumbat@gmail.com>
2024-10-21 12:23:53 +0530

    add a test for the stringer implementation of the tailed list with one element

--- tlistlib/tlistlib_test.go
+++ tlistlib/tlistlib_test.go
@@ -15,3 +15,14 @@ func TestNodeString(t *testing.T) {
 	}
 }
 
+func TestSingleElementTailedListString(t *testing.T) {
+	tl := constructListFromNode(&Node{"a", nil})
+
+	want := "a->nil"
+	got := tl.String()
+
+	if got != want {
+		t.Errorf("*TailedListString.String() on a single element tailed list is incorrect: want '%s', got '%s'", want, got)
+	}
+}
+
