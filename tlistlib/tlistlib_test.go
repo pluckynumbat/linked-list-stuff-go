a5998c0bab67e1c1703796cf5e0769c6c0abc36c
Neehar Patil <pluckynumbat@gmail.com>
2024-10-22 15:19:54 +0530

    added a test for checking the add to end functionality on an empty tailed list

--- tlistlib/tlistlib_test.go
+++ tlistlib/tlistlib_test.go
@@ -70,3 +70,27 @@ func TestIsEmptyFalse(t *testing.T) {
 		t.Errorf("Is Empty on an empty list failed: want '%v', got '%v'", want, got)
 	}
 }
+
+func TestAddToEndOnAnEmptyList(t *testing.T) {
+	tl := &TailedList{}
+	tl.AddToEnd("a")
+
+	want := "a->nil"
+	got := tl.String()
+	if got != want {
+		t.Errorf("Adding a value at the end of an empty list fails: want '%s', got '%s'", want, got)
+	}
+
+	want = "a"
+	got = tl.head.String()
+	if got != want {
+		t.Errorf("New Head on adding a value at the end of an empty list fails: want '%s', got '%s'", want, got)
+	}
+
+	want = "a"
+	got = tl.tail.String()
+	if got != want {
+		t.Errorf("New Tail on adding a value at the end of an empty list fails: want '%s', got '%s'", want, got)
+	}
+}
+
