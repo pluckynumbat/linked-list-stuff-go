430a0449fc5fc87b2417e4004677021f40e92fa8
Neehar Patil <pluckynumbat@gmail.com>
2024-11-01 13:17:59 +0530

    added Stringer implementations for the node and list of the doubly linked list

--- dlistlib/dlistlib.go
+++ dlistlib/dlistlib.go
@@ -5,7 +5,36 @@ type Node struct {
 	next *Node
 
 }
+
+func (n *Node) String() string {
+	if n == nil {
+		return "nil"
+	}
+	return "<-" + n.data + "->"
+}
+
 type DoublyLinkedList struct {
 	head *Node
 	tail *Node
 }
+
+func (dl *DoublyLinkedList) String() string {
+	if dl == nil || dl.head == nil {
+		return "nil"
+	}
+
+	str := "nil<-"
+
+	runner := dl.head
+	for runner != nil {
+		str += runner.data
+		if runner.next != nil {
+			str += "<=>"
+		}
+		runner = runner.next
+	}
+	str += "->nil"
+
+	return str
+}
+
