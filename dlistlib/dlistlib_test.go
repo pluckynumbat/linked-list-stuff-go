d691b99420eb9b57fd1455bf9ff0608252980b86
Neehar Patil <pluckynumbat@gmail.com>
2024-11-01 13:20:07 +0530

    added tests for nil list and single element list String() implementation

--- dlistlib/dlistlib_test.go
+++ dlistlib/dlistlib_test.go
@@ -25,3 +25,25 @@ func TestNodeString(t *testing.T) {
 	}
 }
 
+func TestNilListString(t *testing.T) {
+	var dlist *DoublyLinkedList 
+
+	got := dlist.String()
+	want := "nil"
+
+	if got != want {
+		t.Errorf("Incorrect string for a nil list: want %v, got %v", want, got)
+	}
+}
+
+func TestSingleElementListString(t *testing.T) {
+	node := &Node{nil, "a", nil}
+	dlist := &DoublyLinkedList{node, node}
+	got := dlist.String()
+	want := "nil<-a->nil"
+
+	if got != want {
+		t.Errorf("Incorrect string for a single element list: want %v, got %v", want, got)
+	}
+}
+
