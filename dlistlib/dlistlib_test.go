008423e3846c180d5c8d70381826e4feef385404
Neehar Patil <pluckynumbat@gmail.com>
2024-11-02 12:26:21 +0530

    added a test for the IsNil() method on the doubly linked list

--- dlistlib/dlistlib_test.go
+++ dlistlib/dlistlib_test.go
@@ -12,7 +12,6 @@ func TestNilNodeString(t *testing.T) {
 	if got != want {
 		t.Errorf("Incorrect string for a nil node: want %v, got %v", want, got)
 	}
-
 }
 
 func TestNodeString(t *testing.T) {
@@ -63,3 +62,33 @@ func TestListString(t *testing.T) {
 		t.Errorf("Incorrect string for a single element list: want %v, got %v", want, got)
 	}
 }
+
+func TestIsNil(t *testing.T) {
+	var dlist *DoublyLinkedList
+
+	want := true
+	got := dlist.IsNil()
+
+	if got != want {
+		t.Errorf("Incorrect value of IsNil on nil list: want %v, got %v", want, got)
+	}
+
+	dlist = &DoublyLinkedList{}
+
+	want = false
+	got = dlist.IsNil()
+
+	if got != want {
+		t.Errorf("Incorrect value of IsNil on a non nil list: want %v, got %v", want, got)
+	}
+
+	dlist = new(DoublyLinkedList)
+
+	want = false
+	got = dlist.IsNil()
+
+	if got != want {
+		t.Errorf("Incorrect value of IsNil on a non nil list: want %v, got %v", want, got)
+	}
+}
+
