d2c5c93e3a37187f234a1ee9fc176aedcfb80b5c
Neehar Patil <pluckynumbat@gmail.com>
2024-11-11 12:37:24 +0530

    added a test for removing the last element of a single element doubly linked list

--- dlistlib/dlistlib_test.go
+++ dlistlib/dlistlib_test.go
@@ -729,4 +729,30 @@ func TestRemoveLast(t *testing.T) {
 			fmt.Println(err)
 		}
 	})
+
+	dl = &DoublyLinkedList{}
+	t.Run("Single Element list", func(t *testing.T) {
+		err := dl.AddAtEnd("a")
+		if err != nil {
+			t.Errorf("Adding to a list failed, error: %v", err)
+		} else {
+			removed, err2 := dl.RemoveLast()
+			if err2 != nil {
+				t.Errorf("Removing the last element from a single element list failed, error:  %v", err2)
+			} else {
+
+				want := "a"
+				got := removed.String()
+				if got != want {
+					t.Errorf("Removed element from a list is incorrect, want: %v, got %v", want, got)
+				}
+
+				want = "empty"
+				got = dl.String()
+				if got != want {
+					t.Errorf("Removing the only value from a single element list should result in an empty list")
+				}
+			}
+		}
+	})
 }
