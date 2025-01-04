bbd6d91f0ff53672a1bfc44fdfdcadfc9c775c9f
Neehar Patil <pluckynumbat@gmail.com>
2025-01-04 13:01:03 +0530

    startinf a new library for a semi generic doubly linked list which can contain data of any type that implements the fmt.Stringer interface

---
+++ sdlistlib/sdlistlib.go
@@ -0,0 +1,58 @@
+// Semi generic doubly linked list with nodes that contain
+// data of any type that implements the fmt.Stringer interface
+
+package sdlistlib
+
+import (
+	"fmt"
+)
+
+var nilListError error = fmt.Errorf("The list is nil")
+var emptyListError error = fmt.Errorf("The list is empty")
+
+// Node's data can be anything that implements the Stringer interface
+type Node[T fmt.Stringer] struct {
+	prev *Node[T]
+	data T
+	next *Node[T]
+}
+
+// Node's implementation of the Stringer interface
+func (node *Node[T]) String() string {
+	if node == nil {
+		return "nil"
+	}
+
+	return node.data.String()
+}
+
+type SemiGenericList[T fmt.Stringer] struct {
+	head *Node[T]
+	tail *Node[T]
+}
+
+// Semi Generic List's implementation of the Stringer interface
+func (list *SemiGenericList[T]) String() string {
+	if list.IsNil() {
+		return "nil"
+	}
+
+	if list.IsEmpty() {
+		return "empty"
+	}
+
+	str := "nil<-"
+
+	runner := list.head
+
+	for runner != nil {
+		str += runner.String()
+		if runner.next != nil {
+			str += "<=>"
+		}
+		runner = runner.next
+	}
+	str += "->nil"
+
+	return str
+}
